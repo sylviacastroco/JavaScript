2ef29c2b6f4ef18a1060857809f3e327572b8d40
Sylvia Castro <hello@sylviacastro.co>
2019-06-25 20:40:33 -0500

    Create app.js

---
+++ DOM/rainbowChallenge/app.js
@@ -0,0 +1,7 @@
+const listItems = document.querySelectorAll('#rainbow > li');
+
+var colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
+
+for(var i = 0; i < colors.length; i ++) {
+  listItems[i].style.color = colors[i];    
+}
