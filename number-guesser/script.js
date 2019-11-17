14da931b7e45cb2699c773251c488d81c2151959
Sylvia Castro <hello@sylviacastro.co>
2019-11-16 20:19:13 -0600

    initial commit

---
+++ number-guesser/script.js
@@ -0,0 +1,26 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+//Determine which guess is cloest to the target number
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+  const humanDifference = Math.abs(targetGuess - humanGuess);
+  const computerDifference = Math.abs(targetGuess - computerGuess);
+  return humanDifference <= computerDifference;
+};
+
+//Increase winner's score after each round
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => currentRoundNumber++;
