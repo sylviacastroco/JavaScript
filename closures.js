7f09dac9f5523c65856c2d7535db6ffbdefb349b
sylvia <hello@sylviacastro.co>
2019-12-05 13:54:19 -0600

    added another closure example

--- closures.js
+++ closures.js
@@ -10,3 +10,17 @@ const sayHey = createGreeting('Hey');
 console.log(sayHello('sylvia'));
 console.log(sayHey('jon'));
 console.log(sayHello('doe'));
+
+/* High level closure 
+Closures are the ability for a child function to access variables from a higher level scope even after functions have been closed or closed over*/
+function createGame(gameName) {
+  let score = 0;
+  return function win() {
+    score++;
+    return `Your ${gameName} game score is ${score}`;
+  };
+}
+
+//Because we have created two separate win functions by using the createGame function, they have their own private variable score.
+const hockeyGame = createGame('Hockey');
+const soccerGame = createGame('Soccer');
