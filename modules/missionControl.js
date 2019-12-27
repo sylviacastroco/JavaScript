cc8c57049b9fcfba763c9c707da3c6f58945eb15
Sylvia <sylviacastro527@gmail.com>
2019-12-27 14:06:49 -0600

    practice with modules

---
+++ modules/missionControl.js
@@ -0,0 +1,29 @@
+import {
+  availableAirplanes,
+  flightRequirements,
+  meetsStaffRequirements
+} from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log(
+      'Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity
+    );
+  });
+}
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+    console.log(
+      element.name +
+        ' meets staff requirements: ' +
+        meetsStaffRequirements(
+          element.availableStaff,
+          flightRequirements.requiredStaff
+        )
+    );
+  });
+}
+
+displayFuelCapacity();
+displayStaffStatus();
