342d5c16ec4437dd0288e02d33a63c6247ed521d
Sylvia <sylviacastro527@gmail.com>
2020-01-04 20:47:54 -0600

    setting state of components

--- learn-react/ravenous/src/components/SearchBar/SearchBar.js
+++ learn-react/ravenous/src/components/SearchBar/SearchBar.js
@@ -2,19 +2,38 @@ import React from 'react';
 import './SearchBar.css';
 import '../../images/background_search_desktop.jpg';
 
-const sortByOptions = {
-  'Best Match': 'best_match',
-  'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
-};
-
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    };
+  }
+
+  getSortByClass(sortByOption) {
+    if (this.state.sortBy === sortByOption) {
+      return 'active';
+    } else {
+      return '';
+    }
+  }
+
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
       return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
+
   render() {
     return (
       <div className='SearchBar'>
