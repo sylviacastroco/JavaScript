ad5a152f24541152a048b2363854e245d3a0eb4b
Sylvia <sylviacastro527@gmail.com>
2020-01-05 13:08:21 -0600

    interacting with the Yelp API

--- learn-react/ravenous/src/components/BusinessList/BusinessList.js
+++ learn-react/ravenous/src/components/BusinessList/BusinessList.js
@@ -8,11 +8,10 @@ class BusinessList extends React.Component {
     return (
       <div className='BusinessList'>
         {this.props.businesses.map(business => {
-          return <Business business={business} />;
+          return <Business key={business.id} business={business} />;
         })}
       </div>
     );
   }
 }
-
 export default BusinessList;
--- learn-react/ravenous/src/components/SearchBar/SearchBar.js
+++ learn-react/ravenous/src/components/SearchBar/SearchBar.js
@@ -87,7 +87,9 @@ class SearchBar extends React.Component {
           <input placeholder='Where?' onChange={this.handleLocationChange} />
         </div>
         <div className='SearchBar-submit'>
-          <a onClick={this.handleSearch}>Let's Go</a>
+          <a href='true' onClick={this.handleSearch}>
+            Let's Go
+          </a>
         </div>
       </div>
     );
